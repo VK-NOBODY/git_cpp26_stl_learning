85dd269e689c26b11d03e86e27c6524151d32424
VK-NOBODY <171213515+VK-NOBODY@users.noreply.github.com>
2024-06-18 16:44:26 +0800

    Initial commit

---
+++ cmake-build-release/CMakeFiles/git_cpp26_stl_learning.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_cpp26_stl_learning.dir/main.cpp.obj"
+  "CMakeFiles/git_cpp26_stl_learning.dir/main.cpp.obj.d"
+  "git_cpp26_stl_learning.exe"
+  "git_cpp26_stl_learning.exe.manifest"
+  "git_cpp26_stl_learning.pdb"
+  "libgit_cpp26_stl_learning.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_cpp26_stl_learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
